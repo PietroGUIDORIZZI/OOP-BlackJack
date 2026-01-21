00230cdd963be9b50ec5d90604709a887a8cc665
Pietro Guidorizzi <peterguidorizzi@gmail.com>
2026-01-21 10:26:44 -0300

    OOP Enums toStrings method @override

---
+++ src/test/java/com/bj/blackjack/SuitTest.java
@@ -0,0 +1,14 @@
+package com.bj.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SuitTest {
+
+    @Test
+    void heartPrintsHeart() {
+        assertEquals("\u2665", Suit.HEARTS.toString());
+    }
+
+}
